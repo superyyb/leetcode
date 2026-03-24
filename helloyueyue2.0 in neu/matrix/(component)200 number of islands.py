e8c0f3bed3437679679230291ebd3674c150c1f4
superyyb <superyy0721@gmail.com>
2026-03-24 15:47:16 -0700

    Mar24 Update

--- helloyueyue2.0 in neu/matrix/(component)200 number of islands.py
+++ helloyueyue2.0 in neu/matrix/(component)200 number of islands.py
@@ -9,6 +9,30 @@
         a.if grid[i][j] == 1: call dfs + 计数
         b.return result
 """
+#BFS
+from collections import deque
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    count += 1
+                    q = deque([(i, j)])
+                    grid[i][j] = '0'
+                    while q:
+                        r, c = q.popleft()
+                        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                            nr, nc = r + dr, c + dc
+                            if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == "1":
+                                q.append((nr, nc))
+                                grid[nr][nc] = '0'
+        return count
+
+#DFS
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0
