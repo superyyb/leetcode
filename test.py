56b731c77cc90d7b0d4c760168138f2083395596
superyyb <superyy0721@gmail.com>
2026-03-27 23:56:45 -0700

    Mar27 Update

---
+++ test.py
@@ -0,0 +1,17 @@
+def calculate_sum(nums):
+    #res = sum(nums)
+    total = 0
+    for i in range(len(nums)):
+        total += nums[i] #15
+    return total
+
+#array:[1,1,3,4,5,5,6,3]
+#output:1,3,5
+'''
+Use dictionary to store the frequency of each number
+{1:2, 3:2, 4:1, 5:2, 6:1}
+
+sort:[1,1,3,3,4,5,5,6]g
+for loop nums[n-1] == nums[n]
+'''
+
